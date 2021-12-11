032ff907ccc03782bb47dca7904483bc14b2522e
General <generalwar@outlook.com>
2021-12-11 18:09:14 +0800

    General: remove Background shaders

--- Shaders/Default.cs
+++ Shaders/Default.cs
@@ -44,23 +44,7 @@ namespace Shaders
         public override IFragmentSource FragmentShader => new DefaultTransparentFragmentShader();
     }
 
-    [GraphicsShader("Default/Opaque", RenderType.Opaque, RenderQueue.Geometry)]
-    public class DefaultOpaqueGraphicsShader : GraphicsShader
-    {
-        public override IVertexSource VertexShader => new DefaultVertexShader();
-
-        public override IFragmentSource FragmentShader => new DefaultOpaqueFragmentShader();
-    }
-
-    [GraphicsShader("Default/Background/Transparent", RenderType.Transparent, RenderQueue.Background)]
-    public class BackgroundTransparentGraphicsShader : GraphicsShader
-    {
-        public override IVertexSource VertexShader => new DefaultVertexShader();
-
-        public override IFragmentSource FragmentShader => new DefaultTransparentFragmentShader();
-    }
-
-    [GraphicsShader("Default/Background/Transparent-LineList", RenderType.Transparent, RenderQueue.Background, Polygon = PolygonType.LineList)]
+    [GraphicsShader("Default/Transparent-LineList", RenderType.Transparent, RenderQueue.Transparent, Polygon = PolygonType.LineList)]
     public class BackgroundTransparentLineListGraphicsShader : GraphicsShader
     {
         public override IVertexSource VertexShader => new DefaultVertexShader();
@@ -68,8 +52,8 @@ namespace Shaders
         public override IFragmentSource FragmentShader => new DefaultTransparentFragmentShader();
     }
 
-    [GraphicsShader("Default/Background/Opaque", RenderType.Opaque, RenderQueue.Background)]
-    public class BackgroundOpaqueGraphicsShader : GraphicsShader
+    [GraphicsShader("Default/Opaque", RenderType.Opaque, RenderQueue.Geometry)]
+    public class DefaultOpaqueGraphicsShader : GraphicsShader
     {
         public override IVertexSource VertexShader => new DefaultVertexShader();
 
